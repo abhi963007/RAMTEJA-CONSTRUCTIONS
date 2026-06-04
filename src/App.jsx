197bc1104519e71a73380d045d3a78238fbeaf1c
abhi963007 <ahiramak963@gmail.com>
2026-06-04 10:17:18 +0530

    feat: initialize App component with React Router and layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Projects from './pages/Projects';
-import Pricing from './pages/Pricing';
-import Blogs from './pages/Blogs';
-import Contact from './pages/Contact';
-import ProductsMaster from './pages/ProductsMaster';
-import ProductSingle from './pages/ProductSingle';
-import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -18,15 +9,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Services />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/product-page" element={<ProductsMaster />} />
-          <Route path="/product/:id" element={<ProductSingle />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
     </Router>
